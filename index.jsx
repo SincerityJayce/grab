fa489da75a352ff17f5320a003476aa2650917b8
SincerityJayce <vincetacofield@gmail.com>
2024-03-10 17:32:34 +1100

    initial commit

---
+++ index.jsx
@@ -0,0 +1,17 @@
+
+import { create } from "zustand";
+
+///API
+export const useGrabbed = create((set) => { 
+ let release = () => { set({ card: null }); }
+ let grab =    id => { console.log('I grabbed this:', id); set({ card: id })};
+ window.addEventListener('mouseup', release);
+ 
+ return { card: null, grab, release };
+});
+//////
+export const {grab, release} = useGrabbed.getState()
+export const useAmIGrabbed = (id) => useGrabbed(s => s.card==id)
+
+
+
